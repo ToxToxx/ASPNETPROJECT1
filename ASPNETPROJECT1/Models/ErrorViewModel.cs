6f5c51d957aa12ff5e392c428606de2565ff4a09
Matvey <sinema3d00@gmail.com>
2024-08-10 20:56:59 +0300

    Initial commit

---
+++ ASPNETPROJECT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETPROJECT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
