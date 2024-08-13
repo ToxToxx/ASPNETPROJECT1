d422025cdd876b77d807002c203032893537db0a
Matvey <sinema3d00@gmail.com>
2024-08-13 11:59:01 +0300

    changing design

--- ASPNETPROJECT1/Controllers/HomeController.cs
+++ ASPNETPROJECT1/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ASPNETPROJECT1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
