866fdc41ad6efca15c4c95ce66c894eeccd8e8d4
Matvey <sinema3d00@gmail.com>
2024-08-13 13:05:55 +0300

    made new model for contacts

---
+++ ASPNETPROJECT1/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace ASPNETPROJECT1.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
