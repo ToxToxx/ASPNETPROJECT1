a8aa6277736f2f2ab6ae4f9819a1d654974ae684
Matvey <sinema3d00@gmail.com>
2024-08-13 12:04:23 +0300

    changing css styles

--- ASPNETPROJECT1/Controllers/HomeController.cs
+++ ASPNETPROJECT1/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ASPNETPROJECT1.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
